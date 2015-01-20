cb7f6df2a6f73ff134198dba2d7cc488836f4e48
Sergio Lima <sergiosouzalima@gmail.com>
2015-01-20 12:13:14 +0000

    Added method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
 
+  def last_name
+    # how to query:  j = User.first.to_json :methods => :last_name
+    return "" if name.nil?
+    name.split(" ").last
+  end
+
 end
